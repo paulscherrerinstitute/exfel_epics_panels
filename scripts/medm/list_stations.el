51d013ddd05b9f7088ac66e844a46e0da15a6f6b
Radoslaw Rybaniec <radoslaw.rybaniec@psi.ch>
2019-07-01 15:53:39 +0200

    updated panels, added scripts for generating panels

---
+++ scripts/medm/list_stations.el
@@ -0,0 +1,92 @@
+;; This files contains station lists for medm panels generation
+
+(setq sa1-list '(
+("170" "xfelgpac1di2232t2"  "IPC1446" "BPME-2229-T2"  "BPME-2235-T2" )
+("190" "xfelgpac1di2244sa1" "IPC1423" "BPME-2241-SA1" "BPME-2247-SA1")
+("215" "xfelgpac1di2256sa1" "IPC1431" "BPME-2253-SA1" "BPME-2259-SA1")
+("235" "xfelgpac1di2268sa1" "IPC1428" "BPME-2265-SA1" "BPME-2271-SA1")
+("255" "xfelgpac1di2280sa1" "IPC1509" "BPME-2278-SA1" "BPME-2284-SA1")
+("275" "xfelgpac1di2293sa1" "IPC1440" "BPME-2290-SA1" "BPME-2296-SA1")
+("295" "xfelgpac1di2305sa1" "IPC1486" "BPME-2302-SA1" "BPME-2308-SA1")
+("315" "xfelgpac1di2317sa1" "IPC1445" "BPME-2314-SA1" "BPME-2320-SA1")
+("335" "xfelgpac1di2329sa1" "IPC1441" "BPME-2326-SA1" "BPME-2332-SA1")
+("355" "xfelgpac1di2341sa1" "IPC1496" "BPME-2339-SA1" "BPME-2345-SA1")
+("375" "xfelgpac1di2354sa1" "IPC1554" "BPME-2351-SA1" "BPME-2357-SA1")
+("395" "xfelgpac1di2366sa1" "IPC1620" "BPME-2363-SA1" "BPME-2369-SA1")
+("415" "xfelgpac1di2378sa1" "IPC1600" "BPME-2375-SA1" "BPME-2381-SA1")
+("435" "xfelgpac1di2390sa1" "IPC1621" "BPME-2387-SA1" "BPME-2393-SA1")
+("455" "xfelgpac1di2402sa1" "IPC1541" "BPME-2400-SA1" "BPME-2406-SA1")
+("475" "xfelgpac1di2415sa1" "IPC1617" "BPME-2412-SA1" "BPME-2418-SA1")
+("495" "xfelgpac1di2427sa1" "IPC1544" "BPME-2424-SA1" "BPME-2430-SA1")
+("515" "xfelgpac1di2439sa1" "IPC1618" "BPME-2436-SA1" "BPME-2442-SA1")
+("535" "xfelgpac1di2451sa1" "IPC1613" "BPME-2448-SA1" "BPME-2454-SA1")
+("555" "xfelgpac1di2463sa1" "IPC1635"  nil            "BPME-2461-SA1")))
+
+(setq sa2-list '(
+("170" "xfelgpac1di2194t1"  "IPC1503" "BPME-2191-T1"  "BPME-2197-T1" )                 
+("190" "xfelgpac1di2207sa2" "IPC1494" "BPME-2203-SA2" "BPME-2209-SA2")
+("215" "xfelgpac1di2219sa2" "IPC1487" "BPME-2215-SA2" "BPME-2221-SA2")
+("235" "xfelgpac1di2231sa2" "IPC1548" "BPME-2227-SA2" "BPME-2233-SA2")
+("255" "xfelgpac1di2244sa2" "IPC1470" "BPME-2239-SA2" "BPME-2245-SA2")
+("275" "xfelgpac1di2256sa2" "IPC1507" "BPME-2252-SA2" "BPME-2258-SA2")
+("295" "xfelgpac1di2268sa2" "IPC1543" "BPME-2264-SA2" "BPME-2270-SA2")
+("315" "xfelgpac1di2280sa2" "IPC1502" "BPME-2276-SA2" "BPME-2282-SA2")
+("335" "xfelgpac1di2292sa2" "IPC1499" "BPME-2288-SA2" "BPME-2294-SA2")
+("355" "xfelgpac1di2305sa2" "IPC1525" "BPME-2300-SA2" "BPME-2306-SA2")
+("375" "xfelgpac1di2317sa2" "IPC1508" "BPME-2313-SA2" "BPME-2319-SA2")
+("395" "xfelgpac1di2329sa2" "IPC1511" "BPME-2325-SA2" "BPME-2331-SA2")
+("415" "xfelgpac1di2341sa2" "IPC1606" "BPME-2337-SA2" "BPME-2343-SA2")
+("435" "xfelgpac1di2353sa2" "IPC1519" "BPME-2349-SA2" "BPME-2355-SA2")
+("455" "xfelgpac1di2366sa2" "IPC1516" "BPME-2361-SA2" "BPME-2367-SA2")
+("475" "xfelgpac1di2378sa2" "IPC1624" "BPME-2373-SA2" "BPME-2380-SA2")
+("495" "xfelgpac1di2390sa2" "IPC1539" "BPME-2386-SA2" "BPME-2392-SA2")
+("515" "xfelgpac1di2402sa2" "IPC1492" "BPME-2398-SA2" "BPME-2404-SA2")
+("535" "xfelgpac1di2414sa2" "IPC1491" "BPME-2410-SA2" "BPME-2416-SA2")
+("555" "xfelgpac1di2423sa2" "IPC1571"  nil            "BPME-2422-SA2")))
+
+(setq sa3-list '(
+("170" "xfelgpac1di2793t4"  "IPC1500" "BPME-2794-T4"  "BPME-2800-T4" )                 
+("195" "xfelgpac1di2194t1"  "IPC1503" "BPME-2191-T1"  "BPME-2197-T1" )                                  
+("210" "xfelgpac1di2808sa3" "IPC1447" "BPME-2806-SA3" "BPME-2812-SA3")
+("235" "xfelgpac1di2820sa3" "IPC1450" "BPME-2818-SA3" "BPME-2824-SA3")
+("255" "xfelgpac1di2832sa3" "IPC1444" "BPME-2830-SA3" "BPME-2836-SA3")
+("275" "xfelgpac1di2845sa3" "IPC1538" "BPME-2842-SA3" "BPME-2849-SA3")
+("295" "xfelgpac1di2857sa3" "IPC1522" "BPME-2861-SA3" "BPME-2849-SA3")
+("315" "xfelgpac1di2869sa3" "IPC1430" "BPME-2867-SA3" "BPME-2873-SA3")
+("335" "xfelgpac1di2881sa3" "IPC1421" "BPME-2879-SA3" "BPME-2885-SA3")
+("355" "xfelgpac1di2893sa3" "IPC1434" "BPME-2891-SA3" "BPME-2897-SA3")
+("375" "xfelgpac1di2906sa3" "IPC1476" "BPME-2903-SA3" "BPME-2910-SA3")
+("395" "xfelgpac1di2918sa3" "IPC1506" "BPME-2916-SA3" "BPME-2922-SA3")
+("415" "xfelgpac1di2931sa3" "IPC1498" "BPME-2928-SA3" "BPME-2934-SA3")
+("435" "xfelgpac1di2943sa3" "IPC1588"  nil            "BPME-2940-SA3")))
+
+
+
+(setq ffcol-list '(
+("" "xfelgpac1di1828cl" "IPC1605"  nil           "BPMI-1837-CL" "IBFBYCOL")))
+
+(setq ffb-list '(
+("" "xfelgpac1di1899tl" "IPC1627" "BPMI-1860-TL" "BPMI-1878-TL" "IBFBYUP" (t . t))
+("" "xfelgpac2di1899tl" "IPC1576" "BPMI-1910-TL" "BPMI-1930-TL" "IBFBYDOWN" (nil . t))
+("" "xfelgpac3di1899tl" "IPC1488" "BPMI-1863-TL" "BPMI-1889-TL" "IBFBXUP" (t . t))
+("" "xfelgpac4di1899tl" "IPC1623" "BPMI-1925-TL" "BPMI-1939-TL" "IBFBXDOWN" (nil . t))))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ scripts/medm/panel_generation.el
@@ -0,0 +1,433 @@
+;; this file contains function for generating IBFB panels
+
+(defun medm-text-black (x y text)
+  "insert text object"
+  (insert (format
+"text {
+	object {
+		x=%d
+		y=%d
+        width=200
+		height=20
+	}
+	\"basic attribute\" {
+		clr=14
+	}
+	textix=\"%s\"
+    align=\"horiz. centered\"
+}
+" x y text)))
+
+(defun medm-text-white (x y text)
+  "insert text object"
+  (insert (format
+           "text {
+	object {
+		x=%d
+		y=%d
+	}
+	\"basic attribute\" {
+		clr=0
+	}
+	textix=\"%s\"
+    align=\"horiz. centered\"
+}
+" x y text)))
+
+
+;; text {
+;; object {
+;; x=10
+;; y=213
+;; width=70
+;; height=15
+;; }
+;; "basic attribute" {
+;; clr=0
+;; }
+;; textix="SASE 1"
+;; align="horiz. right"
+;; }
+
+(defun medm-bpm-shell-command (x y bpm-name)
+  "insert shell command"
+  (insert (format
+"\"shell command\" {
+    object {
+        x=%d
+        y=%d
+		width=85
+        height=20
+	}
+    command[0] {
+        label=\"Terminal\"
+        name=\"./xfel_qt.sh %s BPM_GPAC_CAV_SMP.ui &\"
+    }
+    clr=14
+    bclr=57
+    label=\"-%s\"
+}
+"  x y bpm-name bpm-name)))
+
+(defun medm-bpm-serv-message (x y bpm-name host-name port-number)
+  (insert (format
+"\"message button\" {
+object {
+x=%d
+y=%d
+width=85
+height=20
+}
+control {
+chan=\"CAVBPM:BPM-STRING.AA\"
+clr=14
+bclr=57
+}
+label=\"%s\"
+press_msg=\"%s:%d#%s\"
+}
+" x y bpm-name host-name port-number bpm-name)))
+
+
+(defun medm-related-gpac-service (x y dev-name ipc-name)
+  (insert (format 
+"\"related display\" {
+	object {
+		x=%d
+		y=%d
+		width=65
+		height=20
+	}
+	display[0] {
+		label=\"BPM\"
+		name=\"F_DI_FIN250_BPM_GPAC_SERVICE.adl\"
+		args=\"DEV=%s\"
+	}
+	clr=14
+	bclr=57
+	label=\"-%s\"
+}
+" x y (upcase dev-name) (upcase ipc-name))))
+
+(defun medm-related-xfeltim (x y dev-name)
+  (insert (format
+"\"related display\" {
+	object {
+		x=%d
+		y=%d
+		width=65
+		height=20
+	}
+	display[0] {
+		label=\"BPM\"
+		name=\"XFELTIM.adl\"
+		args=\"DEV=%s\"
+	}
+	clr=14
+	bclr=57
+	label=\"-XFELTIM\"
+}
+" x y (upcase dev-name))))
+
+
+(defun medm-xterm-shell-command (x y host-name)
+  "insert xterm shell command"
+  (insert (format
+           "\"shell command\" {
+	object {
+		x=%d
+		y=%d
+		width=115
+		height=20
+	}
+	command[0] {
+		label=\"Terminal\"
+		name=\"xterm -T %s -bg rgb:40/A0/90 -e telnet -l root %s &\"
+	}
+	clr=14
+	bclr=57
+	label=\"-%s\"
+}
+" x y host-name host-name host-name)))
+  
+
+
+
+
+(defun medm-rectangle-red-green-status (x y dev-name)       
+  (insert (format
+"rectangle {
+	object {
+		x=%d
+		y=%d
+		width=5
+		height=20
+	}
+	\"basic attribute\" {
+		clr=20
+	}
+	\"dynamic attribute\" {
+		vis=\"calc\"
+		calc=\"A=0\"
+		chan=\"%s:S7GPAC-STATUS\"
+	}
+}
+rectangle {
+	object {
+		x=%d
+		y=%d
+		width=5
+		height=20
+	}
+	\"basic attribute\" {
+		clr=15
+	}
+	\"dynamic attribute\" {
+		vis=\"calc\"
+		calc=\"A=1\"
+		chan=\"%s:S7GPAC-STATUS\"
+	}
+}
+" x y (upcase dev-name) x y (upcase dev-name))))
+
+(defun medm-rectangle-red-green-status-more (x y dev-name channel-name visibility-red visibility-green)       
+  (when visibility-red
+    (insert (format
+             "rectangle {
+	object {
+		x=%d
+		y=%d
+		width=5
+		height=20
+	}
+	\"basic attribute\" {
+		clr=20
+	}
+	\"dynamic attribute\" {
+		vis=\"calc\"
+		calc=\"%s\"
+		chan=\"%s:%s\"
+	}
+}
+" x y (upcase visibility-red) (upcase dev-name) (upcase channel-name))))
+  (when visibility-green
+    (insert (format
+"rectangle {
+	object {
+		x=%d
+		y=%d
+		width=5
+		height=20
+	}
+	\"basic attribute\" {
+		clr=15
+	}
+	\"dynamic attribute\" {
+		vis=\"calc\"
+		calc=\"%s\"
+		chan=\"%s:%s\"
+	}
+}
+" x y (upcase visibility-green) (upcase dev-name) (upcase channel-name)))))
+
+(defun medm-choice-button (x y dev-name channel)
+  ""
+  (insert (format
+"\"choice button\" {
+	object {
+		x=%d
+		y=%d
+		width=55
+		height=15
+	}
+	control {
+		chan=\"%s:%s\"
+		clr=14
+		bclr=56
+	}
+	stacking=\"column\"
+}
+" x (+ 3 y) (upcase dev-name) (upcase channel))))
+
+(defun medm-text-update (x y dev-name channel)
+  ""
+  (insert (format
+"\"text update\" {
+	object {
+		x=%d
+		y=%d
+		width=45
+		height=15
+	}
+	monitor {
+		chan=\"%s:%s\"
+		clr=14
+		bclr=4
+	}
+	align=\"horiz. right\"
+	limits {
+	}
+}
+" x (+ 3 y) (upcase dev-name) (upcase channel))))
+
+(defun medm-text-entry (x y dev-name channel)
+  ""
+  (insert (format
+"\"text entry\" {
+	object {
+		x=%d
+		y=%d
+		width=45
+		height=15
+	}
+	control {
+		chan=\"%s:%s\"
+		clr=14
+		bclr=4
+	}
+	limits {
+	}
+}
+" x (+ 3 y) (upcase dev-name) (upcase channel))))
+
+(defun medm-insert-line (y bpm-1-name bpm-2-name ipc-name host-name locserv-name &optional hide-gtx-status)
+  ""
+  (let ((locserv-name (if locserv-name locserv-name (if bpm-1-name bpm-1-name bpm-2-name)))
+        ;; this would hide the gtx when name is not existing
+        ;; (bpm-1-gtx-hide (if bpm-1-name (when (consp hide-gtx-status) (car hide-gtx-status)) t))
+        ;; (bpm-2-gtx-hide (if bpm-2-name (when (consp hide-gtx-status) (cdr hide-gtx-status)) t))
+
+        ;; this won't hide gtx status when name is not there
+        (bpm-1-gtx-hide (when (consp hide-gtx-status) (car hide-gtx-status)))
+        (bpm-2-gtx-hide (when (consp hide-gtx-status) (cdr hide-gtx-status))))
+    
+    (when bpm-1-name
+      (medm-bpm-serv-message 90 y bpm-1-name host-name 51236))
+    (when bpm-2-name
+      (medm-bpm-serv-message 175 y bpm-2-name host-name 51238))
+    (medm-related-gpac-service 260 y host-name ipc-name)
+    (medm-rectangle-red-green-status 325 y host-name)
+    (medm-related-xfeltim 330 y host-name)
+    (medm-xterm-shell-command 395 y host-name)
+    (medm-rectangle-red-green-status 510 y locserv-name)
+    (when bpm-1-name (medm-choice-button 520 y locserv-name "BPM1-ENABLED"))
+    (when bpm-2-name (medm-choice-button 580 y locserv-name "BPM2-ENABLED"))
+    (unless bpm-1-gtx-hide (medm-rectangle-red-green-status-more 641 y locserv-name "COM-SFP-STATUS" "(A & 0x100000) > 0" "(A & 0x100000) = 0"))
+    (unless bpm-2-gtx-hide (medm-rectangle-red-green-status-more 646 y locserv-name "COM-SFP-STATUS" "(A & 0x10000000) > 0" "(A & 0x10000000) = 0"))
+    (unless bpm-1-gtx-hide (medm-rectangle-red-green-status-more 660 y locserv-name "BPM1-IBFB-GTX-STATUS" "(A & 0x80) > 0" "(A & 0x80) = 0"))
+    (unless bpm-2-gtx-hide (medm-rectangle-red-green-status-more 665 y locserv-name "BPM2-IBFB-GTX-STATUS" "(A & 0x80) > 0" "(A & 0x80) = 0"))
+    (when bpm-1-name (medm-text-update 692 y locserv-name "BPM1-TRG-DELAY"))
+    (when bpm-2-name (medm-text-update 742 y locserv-name "BPM2-TRG-DELAY"))
+    (when bpm-1-name (medm-text-update 795 y locserv-name "BPM1-BPM-ID"))
+    (when bpm-2-name (medm-text-update 845 y locserv-name "BPM2-BPM-ID"))
+    (when bpm-1-name (medm-text-update 900 y locserv-name "BPM1-ROUTER-ENA"))
+    (when bpm-2-name (medm-text-update 950 y locserv-name "BPM2-ROUTER-ENA"))
+    (when bpm-1-name (medm-text-update 1005 y locserv-name "BPM1-Q-THRESHOLD"))
+    (when bpm-2-name (medm-text-update 1055 y locserv-name "BPM2-Q-THRESHOLD"))
+    (unless bpm-1-gtx-hide (medm-rectangle-red-green-status-more 1120 y locserv-name "BPM1-PING-STATUS" "(A & 0x02) = 0" "(A & 0x02) > 0"))
+    (unless bpm-2-gtx-hide (medm-rectangle-red-green-status-more 1125 y locserv-name "BPM2-PING-STATUS" "(A & 0x02) = 0" "(A & 0x02) > 0"))
+    (unless bpm-1-gtx-hide (medm-rectangle-red-green-status-more 1140 y locserv-name "BPM1-PING-STATUS" nil "(A & 0x08) > 0"))
+    (unless bpm-2-gtx-hide (medm-rectangle-red-green-status-more 1145 y locserv-name "BPM2-PING-STATUS" nil "(A & 0x08) > 0"))
+    ))
+                    
+;; (defun medm-test ()
+;;   "add test"
+;;   (interactive)
+;;   (with-temp-file "test.adl"
+;;     (insert-file-contents "test_template.adl")
+;;     (goto-char (point-max))
+;;     (insert "\n")
+;;     (let ((y-offset 40))
+;;       (let ((y (+ y-offset 55)))
+;;         (dolist (line ffcol-list)
+;;           (medm-insert-line y (fourth line) (fifth line) (third line) (second line) (sixth line))
+;;           (setq y (+ 20 y))))
+;;       (let ((y (+ y-offset 85)))
+;;         (dolist (line ffb-list)
+;;           (medm-insert-line y (fourth line) (fifth line) (third line) (second line) (sixth line) (seventh line))
+;;           (setq y (+ 20 y))))
+;;       (let ((y (+ y-offset 175)))
+;;         (dolist (line sa1-list)
+;;           (medm-insert-line y (fourth line) (fifth line) (third line) (second line) nil)
+;;           (setq y (+ 20 y))))
+;;       (let ((y (+ y-offset 565)))
+;;         (dolist (line sa2-list)
+;;           (medm-insert-line y (fourth line) (fifth line) (third line) (second line) nil)
+;;           (setq y (+ 20 y))))
+;;       (let ((y (+ y-offset 955)))
+;;         (dolist (line sa3-list)
+;;           (medm-insert-line y (fourth line) (fifth line) (third line) (second line) nil)
+;;           (setq y (+ 20 y)))))))
+
+(defun ibfb-xfel ()
+  "generate ibfb-xfel panels"
+  (interactive)
+  (loop
+   for sase-name in '("SA1" "SA2" "SA3")
+   for mbu-list-name in (list sa1-list sa2-list sa3-list)
+   do
+   (with-temp-file (concat "IBFBCAV_" sase-name ".adl")
+     (insert-file-contents "IBFBCAV_SA_template.adl")
+     (goto-char (point-max))
+     (insert "\n")
+     (medm-text-black 445 10 (concat "IBFB BPMs SASE " sase-name))
+     (medm-text-white 40 413 (concat "SASE " sase-name))
+     (let ((y-offset 40))
+       (let ((y (+ y-offset 55)))
+         (dolist (line ffcol-list)
+           (medm-insert-line y (fourth line) (fifth line) (third line) (second line) (sixth line))
+           (setq y (+ 20 y))))
+       (let ((y (+ y-offset 85)))
+         (dolist (line ffb-list)
+           (medm-insert-line y (fourth line) (fifth line) (third line) (second line) (sixth line) (seventh line))
+           (setq y (+ 20 y))))
+       (let ((y (+ y-offset 175)))
+         (dolist (line mbu-list-name)
+           (medm-insert-line y (fourth line) (fifth line) (third line) (second line) nil)
+           (setq y (+ 20 y))))))))
+       
+
+(defun ibfbcav-psi ()
+  "add test"
+  (interactive)
+  (let ((ibfbcav-psi-list '(
+                            ("210" "xfelgpac1di1899tl" "IPC1627" "BPMI-1860-TL" "BPMI-1878-TL")
+                            ("235" "xfelgpac2di1899tl" "IPC1576" "BPMI-1910-TL" "BPMI-1930-TL")
+                            ("255" "xfelgpac3di1899tl" "IPC1488" "BPMI-1863-TL" "BPMI-1889-TL")
+                            ("275" "ipc1629"           "IPC1629" "IPC1629-IBFB" "IPC1629-IBFB")
+                            ("212" "ipc1536"           "IPC1536" "IPC1536-IBFB" "IPC1536-IBFB") 
+
+                            )))
+    (with-temp-file "IBFBCAV_PSI.adl"
+      (insert-file-contents "IBFBCAV_PSI_template.adl")
+      (goto-char (point-max))
+      (insert "\n")
+      (let ((y-offset 40))
+        (let ((y (+ y-offset 55)))
+          (dolist (line ibfbcav-psi-list)
+            (medm-insert-line y (fourth line) (fifth line) (third line) (second line) (sixth line))
+            (setq y (+ 20 y))))))))
+
+
+
+(defun bpmsetid-test ()
+  "generate list for setbpm"
+  (interactive)
+  (switch-to-buffer "bpmsetid-test")
+  (erase-buffer)
+  (let ((bpmid 50))
+    (dolist (line sa1-list)
+      (insert (format "%s %d %d\n" (second line) bpmid (+ 1 bpmid)))
+      (setq bpmid (+ 2 bpmid))))
+  (insert "\n")
+  (let ((bpmid 100))
+    (dolist (line sa2-list)
+      (insert (format "%s %d %d\n" (second line) bpmid (+ 1 bpmid)))
+      (setq bpmid (+ 2 bpmid))))
+  (insert "\n")
+  (let ((bpmid 150))
+    (dolist (line sa3-list)
+      (insert (format "%s %d %d\n" (second line) bpmid (+ 1 bpmid)))
+      (setq bpmid (+ 2 bpmid)))))
+
+
