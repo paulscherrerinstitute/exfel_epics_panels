7a6aad7e87c5001a2bdbda19ad7c8c2299f98dba
PSI <psiepics@xfelpsiibfb.desy.de>
2021-03-16 08:32:33 +0100

    Improved development scripts

--- start_medm.sh
+++ start_medm.sh
@@ -1,3 +1,15 @@
-medm -x -dg +0+0 App/config/medm/XFEL.adl
 
+if [[ $1 == "--help" ]]; then
+	echo "usage: start_medm.sh [--devl]"
+	exit 0
+fi;
+
+if [[ $1 == "--devl" ]]; then
+	echo "MEDM development mode started..."
+	params=""
+else
+	params="-x -dg +0+0" 
+fi;
+
+medm $params App/config/medm/XFEL.adl &
 
--- xfel_qt.sh
+++ xfel_qt.sh
@@ -1,5 +1,5 @@
-export CAQTDM_DISPLAY_PATH=/xfel/controls/App/config/qt
+#export CAQTDM_DISPLAY_PATH=/xfel/controls/App/config/qt
 cd App/config/qt
 echo PARAM1 $1
-startDM -noMsg -attach -macro DEV=$1 $2 &
+caqtdm -noMsg -attach -macro DEV=$1 $2 &
 
